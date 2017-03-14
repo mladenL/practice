0e7bc2b166b05766d99383bbee6e85a0b92a30d7
mladen <mladen.ocr@gmail.com>
2017-03-14 10:04:04 +0100

    Posts in chat.php are now collected from newer to older.

--- chat.php
+++ chat.php
@@ -4,7 +4,7 @@
 
 //AFFICHAGE DES POSTS
 
-$req_MSG = $bdd->query('SELECT post_date, user_name, post_content FROM users INNER JOIN messages ON user_id = post_author');
+$req_MSG = $bdd->query('SELECT post_date, user_name, post_content FROM users INNER JOIN messages ON user_id = post_author ORDER BY post_id DESC');
 
 while ($MSG = $req_MSG->fetch()) {
 	echo '<p><span style="font-weight: bold; color: grey;">[' . $MSG['post_date'] . '] - ' . strip_tags($MSG['user_name']) . ' a dit : </span>' . strip_tags($MSG['post_content']) . '</p>';
